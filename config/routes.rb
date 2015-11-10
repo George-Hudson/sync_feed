8d042c525835807111477a5d61a2a7b5344a4269
George Hudson <georgehudson78@gmail.com>
2015-11-10 12:55:42 -0700

    changed routes to reflect actual client route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :sync_data, only: [:index], defaults: { format: :json}
+      get "/sync/summary", to: 'sync_data#index', defaults: { format: :json }
     end
   end
 
